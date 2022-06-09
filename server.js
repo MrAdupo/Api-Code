69106fc2cc588792e3c859055df9c3826d335b9d
MrAdupo <106027174+MrAdupo@users.noreply.github.com>
2022-06-08 19:53:37 -0700

    fix delete and add user

--- server.js
+++ server.js
@@ -1,64 +1,61 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const db = require(__dirname + "/" + "records.json")
- 
+let db = require(__dirname + "/" + "records.json")
+app.use(express.json())
+
 
 
 //Return All The Records In The Data
 app.get('/users', (req, res) => {
-    res.send(db) 
-     })
+    res.send(db)
+})
 
 
 //Return Record Per Id
 app.get('/user/:id', (req, res) => {
     const id = +req.params.id
-    const results =  db.find(item => {
+    const result = db.find(item => {
         return item.id === id
     })
-     res.send(results)
-        })
+    res.send(result)
+})
 
 // This code updates user records
-app.use(express.json())
 app.patch('/updateUser/:id', (req, res) => {
-    const mm = req.body
+    const newUserDetails = req.body
     const id = +req.params.id
-    console.log(mm)
-        const data2 = db.findIndex(a => a.id === id)
-        console.log("before update" , db[data2])
-        db[data2] = mm
-        res.send (db[data2])
-        
-         })
+    const data2 = db.findIndex(a => a.id === id)
+    console.log("before update", db[data2])
+    db[data2] = newUserDetails
+    res.send(db[data2])
+
+})
 
 // this code deletes user  by id 
- app.delete('/deleteUser/:id', (req,res) =>{
+app.delete('/deleteUser/:id', (req, res) => {
     const id = +req.params.id
-    db.splice( (i) => i.id == id)
+    db = db.filter((i) => i.id !== id)
     res.status(200).send()
-          })
-    
+})
+
 
 //This code adds new user
-app.use(express.json())
-app.post('/createUser', (req, res) => { 
-   const uuid = Math.floor(Math.random() * 100)
-      const final = db.push(uuid,req.body) 
-      if(final){
-        console.log(final)
-      }
-      
+app.post('/createUser', (req, res) => {
+    const uuid = Math.floor(Math.random() * 100)
+    const newUser = { id: uuid, ...req.body }
+    db.push(newUser)
+    res.send(newUser)
+
     // db.forEach(b => b.id === id + 1)
- 
+
     // res.send(db)
 
-       }) 
- 
+})
+
 
 // This code creates a listening port on 3000 OR an acceptable one for Heroku
-   app.listen(port, () => {
+app.listen(port, () => {
     console.log("started server on port " + port)
-   }
-    )
+}
+)
